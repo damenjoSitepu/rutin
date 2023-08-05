20ce5b0bd22dffd6686099a68c5f563fc29fc0a9
Damenjo Sitepu <damenstory@gmail.com>
2023-08-05 10:43:49 +0700

    Feaet: RME ( RutinMessageException ) for message list

---
+++ src/Exceptions/RME.php
@@ -0,0 +1,9 @@
+<?php 
+namespace Damenjo\Rutin\Exceptions;
+
+abstract class RME {
+    public const TIMEZONE_EMPTY = "Timezone argument cannot be empty!";
+    public const FORMAT_EMPTY = "Format argument cannot be empty!";
+    public const PREDICTION_MUST_BE_BOOLEAN_RETURNED = "The callback prediction must return a boolean value!";
+    public const MESSAGE_NAME_NOT_FOUND = "Error message name are not found!";
+}
